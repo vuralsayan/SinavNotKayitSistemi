c70616093bec4f7af29fe76c499b6d2306a58ab3
Vural Sayan <vuralsayan01@gmail.com>
2023-07-18 19:18:31 +0300

    Giriş yapılan öğrenci bilgileri FrmOgrenciDetay sayfasına aktarıldı

--- Not_Kayit_Sistemi/FrmOgrenciDetay.cs
+++ Not_Kayit_Sistemi/FrmOgrenciDetay.cs
@@ -19,10 +19,25 @@ namespace Not_Kayit_Sistemi
         }
 
         public string numara;
-        SqlConnection baglanti = new SqlConnection(@"Data Source=Vural\\SQLEXPRESS;Initial Catalog=DbNotKayit;Integrated Security=True");
+        SqlConnection baglanti = new SqlConnection(@"Data Source=Vural\SQLEXPRESS;Initial Catalog=DbNotKayit;Integrated Security=True");
         private void FrmOgrenciDetay_Load(object sender, EventArgs e)
         {
             LblNumara.Text = numara;
+
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("SELECT * FROM TBLDERS WHERE OGRNUMARA=@P1",baglanti);
+            komut.Parameters.AddWithValue("@P1", numara);   
+            SqlDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                LblAdSoyad.Text = dr[2].ToString() + " " + dr[3].ToString();
+                LblSınav1.Text = dr[4].ToString();  
+                LblSınav2.Text = dr[5].ToString();
+                LblSınav3.Text = dr[6].ToString();
+                LblOrtalama.Text = dr[7].ToString();
+                LblDurum.Text = dr[8].ToString();
+            }
+            baglanti.Close();
         }
     }
 }
