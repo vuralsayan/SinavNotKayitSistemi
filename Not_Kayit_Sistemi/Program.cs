9bf76dc2f0816698352e3284de1b25416b738390
Vural Sayan <vuralsayan01@gmail.com>
2023-07-18 18:30:49 +0300

    Giriş formu ismi düzenlendi

--- Not_Kayit_Sistemi/Program.cs
+++ Not_Kayit_Sistemi/Program.cs
@@ -16,7 +16,7 @@ namespace Not_Kayit_Sistemi
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new OgrenciGiris());
         }
     }
 }
